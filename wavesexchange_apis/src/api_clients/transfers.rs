915d95fea61f7d558a2006f8683fc15905919f7e
Artyom Sidorenko <asidorenko@web3tech.ru>
2022-02-06 21:21:44 +0500

    use serde_qs in transfers api

--- wavesexchange_apis/src/api_clients/transfers.rs
+++ wavesexchange_apis/src/api_clients/transfers.rs
@@ -1,6 +1,5 @@
 use crate::{ApiResult, BaseApi, HttpClient};
 use async_recursion::async_recursion;
-use chrono::{DateTime, Utc};
 use wavesexchange_warp::pagination::List;
 
 #[derive(Clone, Debug)]
@@ -10,58 +9,11 @@ impl BaseApi for TransfersApi {}
 
 impl HttpClient<TransfersApi> {
     #[async_recursion]
-    pub async fn get(&self, req: SearchTransfersRequest) -> ApiResult<List<dto::TransferResponse>> {
-        let mut query_params = vec![];
-
-        if let Some(senders) = &req.senders {
-            query_params.push(
-                senders
-                    .iter()
-                    .map(|sender| format!("sender__in[]={}", sender))
-                    .collect::<Vec<String>>()
-                    .join("&"),
-            );
-        }
-
-        if let Some(recipient) = &req.recipient {
-            query_params.push(format!("recipient={}", recipient));
-        }
-
-        if let Some(asset_id) = &req.asset_id {
-            query_params.push(format!("asset_id={}", asset_id));
-        }
-
-        if let Some(asset_ids) = &req.asset_id_in {
-            query_params.push(
-                asset_ids
-                    .iter()
-                    .map(|asset_id| format!("asset_id__in[]={}", asset_id))
-                    .collect::<Vec<String>>()
-                    .join("&"),
-            );
-        }
-
-        if let Some(attachment_utf8_match) = &req.attachment_utf8_match {
-            query_params.push(format!("attachment_utf8__match={}", attachment_utf8_match));
-        }
-
-        if let Some(block_timestamp_gte) = &req.block_timestamp_gte {
-            query_params.push(format!("block_timestamp__gte={}", block_timestamp_gte));
-        }
-
-        if let Some(block_timestamp_lt) = &req.block_timestamp_lt {
-            query_params.push(format!("block_timestamp__lt={}", block_timestamp_lt));
-        }
-
-        if let Some(limit) = &req.limit {
-            query_params.push(format!("limit={}", limit));
-        }
-
-        if let Some(after) = &req.after {
-            query_params.push(format!("after={}", after));
-        }
-
-        let request_url = format!("transfers?{}", query_params.join("&"));
+    pub async fn get(
+        &self,
+        req: dto::SearchTransfersRequest,
+    ) -> ApiResult<List<dto::TransferResponse>> {
+        let request_url = format!("transfers?{}", serde_qs::to_string(&req).unwrap());
 
         self.create_req_handler(self.http_get(&request_url), "transfers::get")
             .execute()
@@ -76,21 +28,8 @@ pub struct Transfer {
     pub attachment_utf8: Option<String>,
 }
 
-#[derive(Clone, Debug)]
-pub struct SearchTransfersRequest {
-    pub senders: Option<Vec<String>>,
-    pub recipient: Option<String>,
-    pub asset_id: Option<String>,
-    pub asset_id_in: Option<Vec<String>>,
-    pub attachment_utf8_match: Option<String>,
-    pub block_timestamp_gte: Option<DateTime<Utc>>,
-    pub block_timestamp_lt: Option<DateTime<Utc>>,
-    pub limit: Option<i64>,
-    pub after: Option<String>,
-}
-
 pub mod dto {
-    use chrono::{DateTime, FixedOffset};
+    use chrono::{DateTime, FixedOffset, Utc};
     use serde::{Deserialize, Serialize};
 
     #[derive(Clone, Debug, Deserialize, Serialize)]
@@ -112,14 +51,18 @@ pub mod dto {
         pub attachment: Option<String>,
         pub attachment_utf8: Option<String>,
     }
-}
 
-impl From<&dto::TransferResponse> for Transfer {
-    fn from(t: &dto::TransferResponse) -> Self {
-        Transfer {
-            asset_id: t.asset_id.to_owned(),
-            amount: t.amount,
-            attachment_utf8: t.attachment_utf8.to_owned(),
-        }
+    #[allow(non_snake_case)]
+    #[derive(Clone, Debug, Serialize)]
+    pub struct SearchTransfersRequest {
+        pub sender__in: Option<Vec<String>>,
+        pub recipient: Option<String>,
+        pub asset_id: Option<String>,
+        pub asset_id__in: Option<Vec<String>>,
+        pub attachment_utf8__match: Option<String>,
+        pub block_timestamp__gte: Option<DateTime<Utc>>,
+        pub block_timestamp__lt: Option<DateTime<Utc>>,
+        pub limit: Option<i64>,
+        pub after: Option<String>,
     }
 }
