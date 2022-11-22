7b1557f5d71d438dc451b7a89b4841e741a22621
Artyom Sidorenko <asidorenko@web3tech.ru>
2022-11-23 01:03:42 +0300

    rest

--- wavesexchange_repos/src/circuit_breaker.rs
+++ wavesexchange_repos/src/circuit_breaker.rs
@@ -5,56 +5,105 @@
 разрывы соединений:
     разные бд (pg, redis), разные пулы (bb8, deadpool, r2d2), одиночный запрос, возможность расширения
 */
-use derive_builder::Builder;
 use std::{
     future::Future,
     num::NonZeroUsize,
     time::{Duration, Instant},
 };
 
-#[derive(Debug, Builder)]
-#[builder(pattern = "owned")]
-pub struct CircuitBreaker<Repo: FallibleRepo> {
-    repo: Repo,
-    #[builder(setter(skip))]
+pub struct CircuitBreaker<S: FallibleDataSource> {
+    data_source: S,
     err_count: usize, // current errors count
-    #[builder(setter(skip))]
     first_err_ts: Option<Instant>,
     max_timespan: Duration, // максимальный временной промежуток, в котором будут считаться ошибки
-    #[builder(setter(custom))]
     max_err_count_per_timespan: NonZeroUsize,
+    init_fn: Box<dyn Fn() -> S>,
 }
 
-impl<Repo: FallibleRepo> CircuitBreakerBuilder<Repo> {
-    pub fn max_err_count_per_timespan(mut self, ts: usize) -> CircuitBreakerBuilder<Repo> {
+pub struct CircuitBreakerBuilder<S: FallibleDataSource> {
+    max_timespan: Option<Duration>,
+    max_err_count_per_timespan: Option<NonZeroUsize>,
+    init_fn: Option<Box<dyn Fn() -> S>>,
+}
+
+impl<S: FallibleDataSource> CircuitBreakerBuilder<S> {
+    pub fn new() -> CircuitBreakerBuilder<S> {
+        CircuitBreakerBuilder {
+            max_timespan: None,
+            max_err_count_per_timespan: None,
+            init_fn: None,
+        }
+    }
+
+    pub fn max_timespan(mut self, ts: usize) -> CircuitBreakerBuilder<S> {
         self.max_err_count_per_timespan = NonZeroUsize::new(ts);
         self
     }
-}
 
-impl<Repo: FallibleRepo> CircuitBreaker<Repo> {
-    pub fn builder() -> CircuitBreakerBuilder<Repo> {
-        CircuitBreakerBuilder::default()
+    pub fn max_err_count_per_timespan(mut self, count: usize) -> CircuitBreakerBuilder<S> {
+        self.max_err_count_per_timespan = NonZeroUsize::new(count);
+        self
     }
 
-    pub async fn query<T, F, Fut>(&mut self, query_fn: F) -> Result<T, Repo::Error>
+    pub fn init_fn(mut self, f: impl Fn() -> S + 'static) -> CircuitBreakerBuilder<S> {
+        self.init_fn = Some(Box::new(f));
+        self
+    }
+
+    pub fn build(self) -> Result<CircuitBreaker<S>, Error> {
+        let build_err = |s: &str| Err(Error::BuilderError(s.to_string()));
+
+        if self.max_err_count_per_timespan.is_none() {
+            return build_err("max_err_count_per_timespan is not set");
+        }
+
+        if self.max_timespan.is_none() {
+            return build_err("max_timespan is not set");
+        }
+
+        if self.init_fn.is_none() {
+            return build_err("init_fn is not set");
+        }
+
+        let init_fn = self.init_fn.unwrap();
+
+        Ok(CircuitBreaker {
+            data_source: init_fn(),
+            err_count: 0,
+            first_err_ts: None,
+            max_timespan: self.max_timespan.unwrap(),
+            max_err_count_per_timespan: self.max_err_count_per_timespan.unwrap(),
+            init_fn,
+        })
+    }
+}
+
+impl<S: FallibleDataSource> CircuitBreaker<S> {
+    pub fn builder() -> CircuitBreakerBuilder<S> {
+        CircuitBreakerBuilder::new()
+    }
+
+    pub async fn query<T, F, Fut>(&mut self, query_fn: F) -> Result<T, S::Error>
     where
-        F: Fn(&Repo) -> Fut,
-        Fut: Future<Output = Result<T, Repo::Error>>,
+        F: Fn(&S) -> Fut,
+        Fut: Future<Output = Result<T, S::Error>>,
     {
-        let result = query_fn(&self.repo).await;
+        let result = query_fn(&self.data_source).await;
 
         if let Err(e) = &result {
-            if Repo::is_countable_err(e) {
+            if S::is_countable_err(e) {
                 self.err_count += 1;
                 match self.first_err_ts {
                     Some(ts) if ts.elapsed() <= self.max_timespan => {
                         if self.err_count > self.max_err_count_per_timespan.get() {
-                            return Err(self.repo.fallback());
+                            return Err(self.data_source.fallback());
                         }
                     }
                     None => self.first_err_ts = Some(Instant::now()),
                     _ => {}
+                }
+                if S::REINIT_ON_FAIL {
+                    self.data_source = (self.init_fn)();
                 }
             }
         } else {
@@ -66,10 +115,9 @@ impl<Repo: FallibleRepo> CircuitBreaker<Repo> {
 }
 
 #[async_trait]
-pub trait FallibleRepo {
+pub trait FallibleDataSource {
+    const REINIT_ON_FAIL: bool;
     type Error;
-
-    async fn init<C>(cfg: &C) -> Self;
 
     fn is_countable_err(err: &Self::Error) -> bool;
 
@@ -80,6 +128,20 @@ pub trait FallibleRepo {
 
 pub mod impls {
     use super::*;
+    use deadpool_diesel::{Manager, Pool};
+    use diesel::pg::PgConnection;
+    use diesel::result::Error as DslError;
+
+    pub struct DeadpoolPgBreaker(Pool<Manager<PgConnection>>);
+
+    impl FallibleDataSource for DeadpoolPgBreaker {
+        const REINIT_ON_FAIL: bool = true;
+        type Error = DslError;
+
+        fn is_countable_err(err: &Self::Error) -> bool {
+            err.to_string().contains("no connection to the server")
+        }
+    }
 }
 
 #[derive(Debug, thiserror::Error)]
