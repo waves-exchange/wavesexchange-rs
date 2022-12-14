6426614863d0e3c65c14d6ad53eee1d62a8cfeff
Alex Kordys <akordys@web3tech.ru>
2022-12-14 14:06:58 +0300

    Reexport bigdecimal

--- wavesexchange_apis/src/lib.rs
+++ wavesexchange_apis/src/lib.rs
@@ -7,5 +7,8 @@ pub mod models;
 pub use clients::{grpc::GrpcClient, http::HttpClient};
 pub use error::{ApiResult, Error};
 
-// reexport api structs
+// Reexport api structs
 pub use api_clients::*;
+
+// Reexport bigdecimal
+pub use bigdecimal;
