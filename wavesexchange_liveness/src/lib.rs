ac406b25bf696f57b8b172ed7dd71f33e4c1f5fa
Alex Kordys <akordys@web3tech.ru>
2024-03-14 11:49:14 +0300

    wavesexchange_liveness v0.4.1 * Less annoying logging: only log when health status changes, not every second

--- wavesexchange_liveness/src/lib.rs
+++ wavesexchange_liveness/src/lib.rs
@@ -1,5 +1,7 @@
 //! Liveness probe based on periodic Postgres query check
 
+extern crate wavesexchange_log as log;
+
 #[cfg(all(feature = "diesel1", not(feature = "diesel2")))]
 extern crate diesel1 as diesel; // Diesel 1.x
 
@@ -15,7 +17,6 @@ use diesel::{
 };
 use std::time::{Duration, Instant};
 use tokio::{sync::mpsc, task, time};
-use wavesexchange_log::{debug, error};
 use wavesexchange_warp::endpoints::Readiness;
 
 const LAST_BLOCK_TIMESTAMP_QUERY: &str = "SELECT time_stamp FROM blocks_microblocks WHERE time_stamp IS NOT NULL AND time_stamp != 0 ORDER BY uid DESC LIMIT 1";
@@ -47,13 +48,26 @@ pub fn channel(
     let query = custom_query.unwrap_or(LAST_BLOCK_TIMESTAMP_QUERY.to_string());
 
     task::spawn(async move {
-        loop {
-            let send = |status: Readiness| {
-                if readiness_tx.send(status).is_err() {
-                    error!("Failed to send {:?} status", status);
+        let mut send = {
+            let mut last_status = Readiness::Ready;
+            let mut last_time = None;
+            move |status: Readiness, timestamp: Option<i64>| {
+                if status != last_status {
+                    if let Some(timestamp) = timestamp {
+                        log::debug!("Current timestamp: {}", timestamp);
+                    }
+                    #[rustfmt::skip]
+                    log::debug!("Sending status: {:?} (prev status was {:?} at time {:?})", status, last_status, last_time);
                 }
-            };
+                if readiness_tx.send(status).is_err() {
+                    log::error!("Failed to send {:?} status", status);
+                }
+                last_status = status;
+                last_time = timestamp;
+            }
+        };
 
+        loop {
             time::sleep(Duration::from_secs(poll_interval_secs)).await;
 
             match PgConnection::establish(&db_url) {
@@ -65,37 +79,31 @@ pub fn channel(
                     match query_result {
                         Ok(last_block_timestamp) => {
                             if let Some(timestamp) = last_block_timestamp {
-                                debug!("Current timestamp: {}", timestamp);
                                 let now = Instant::now();
                                 if timestamp > last_block.timestamp {
                                     last_block.timestamp = timestamp;
                                     last_block.last_change = now;
-                                    debug!("Sending status: Ready");
-                                    send(Readiness::Ready);
+                                    send(Readiness::Ready, last_block_timestamp);
                                 } else {
                                     if now.duration_since(last_block.last_change) > max_block_age {
-                                        debug!("Sending status: Dead");
-                                        send(Readiness::Dead);
+                                        send(Readiness::Dead, last_block_timestamp);
                                     } else {
-                                        debug!("Sending status: Ready");
-                                        send(Readiness::Ready);
+                                        send(Readiness::Ready, last_block_timestamp);
                                     }
                                 }
                             } else {
-                                error!("Could not get last block timestamp");
-                                debug!("Sending status: Ready");
-                                send(Readiness::Ready);
+                                log::error!("Could not get last block timestamp");
+                                send(Readiness::Ready, last_block_timestamp);
                             }
                         }
                         Err(err) => {
-                            error!("Error while fetching last block timestamp: {}", err);
-                            debug!("Sending status: Dead");
-                            send(Readiness::Dead);
+                            log::error!("Error while fetching last block timestamp: {}", err);
+                            send(Readiness::Dead, None);
                         }
                     }
                 }
                 Err(err) => {
-                    error!("Error establishing database connection: {}", err);
+                    log::error!("Error establishing database connection: {}", err);
                 }
             }
         }
